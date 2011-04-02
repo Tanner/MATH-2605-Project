53a1206bd5bf7d2c18af78d3c4778de0c4ca6d52
Tanner Smith <tanner@tssoftware.net>
2011-04-02 12:08:10 -0400

    Reformat generateRandomMatries method to just generate one matrix and run it multiple times in the main method to generate x number of matrices. Create method headers for powerMethod and inversePowerMethod.

--- src/powermethod/PowerMethodMain.java
+++ src/powermethod/PowerMethodMain.java
@@ -9,18 +9,15 @@ public class PowerMethodMain {
 	public static void main(String[] args) {
 		ArrayList<Matrix> matrices = new ArrayList<Matrix>();
 		
-		generateRandomMatrices(matrices, 1000, -2, 2);
+		for (int i = 0; i < 1000; i++) {
+			generateRandomMatrix(matrices, -2, 2);
+		}
 	}
 	
-	public static void generateRandomMatrices(ArrayList<Matrix> matrices, int amount, int lowerBound, int upperBound) {
-		//Generate a number of 2×2 matrices with random components evenly distributed in an interval [a,b]
+	public static void generateRandomMatrix(ArrayList<Matrix> matrices, int lowerBound, int upperBound) {
+		//Generate a 2×2 matrix with random components evenly distributed in an interval [a,b]
 		if (matrices == null) {
 			matrices = new ArrayList<Matrix>();
-		}
-		
-		if (amount <= 0) {
-			return;
-			//throw new Exception("Amount not a positive number.");
 		}
 		
 		if (lowerBound > upperBound) {
@@ -30,14 +27,20 @@ public class PowerMethodMain {
 		
 		Random rand = new Random();
 		
-		for (int i = 0; i < amount; i++) {
-			double[][] matrix = new double[2][2];
-			for (int r = 0; r < matrix.length; r++) {
-				for (int c = 0; c < matrix[r].length; c++) {
-					matrix[r][c] = rand.nextInt(upperBound - lowerBound + 1) + lowerBound;
-				}
+		double[][] matrix = new double[2][2];
+		for (int r = 0; r < matrix.length; r++) {
+			for (int c = 0; c < matrix[r].length; c++) {
+				matrix[r][c] = rand.nextInt(upperBound - lowerBound + 1) + lowerBound;
 			}
-			matrices.add(new Matrix(matrix));
 		}
+		matrices.add(new Matrix(matrix));
+	}
+	
+	public static void powerMethod(Matrix matrix) {
+		//
+	}
+	
+	public static void inversePowerMethod(Matrix matrix) {
+		//
 	}
 }
