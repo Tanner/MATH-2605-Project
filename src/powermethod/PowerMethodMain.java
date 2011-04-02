2eaaa36aeb23d8ab073bb630e160f74c098d7c5d
Tanner Smith <tanner@tssoftware.net>
2011-04-02 15:49:55 -0400

    Begin to fill in the powerMethod. Create Rayleigh Equation method to solve for the eigenvalue from a matrix and eigenvector. Create a test case in the main method.

--- src/powermethod/PowerMethodMain.java
+++ src/powermethod/PowerMethodMain.java
@@ -12,6 +12,10 @@ public class PowerMethodMain {
 		for (int i = 0; i < 1000; i++) {
 			generateRandomMatrix(matrices, -2, 2);
 		}
+		
+		Matrix testMatrix = new Matrix(new double[][]{{1, 2},{3, 4}});
+		testMatrix.print(2, 2);
+		powerMethod(testMatrix, 1);
 	}
 	
 	public static void generateRandomMatrix(ArrayList<Matrix> matrices, int lowerBound, int upperBound) {
@@ -36,11 +40,31 @@ public class PowerMethodMain {
 		matrices.add(new Matrix(matrix));
 	}
 	
-	public static void powerMethod(Matrix matrix) {
-		//
+	public static void powerMethod(Matrix matrix, int desiredAccuracy) {
+		Matrix approximation = new Matrix(new double[][]{{1},{1}});
+		
+		int accuracy = 0;
+		int iterations = 0;
+		//while (accuracy <= desiredAccuracy)
+		//{
+			approximation = matrix.times(approximation);
+			
+			//Use's the Rayleigh Equation to solve for the eignvalue
+			double eigenvalue = rayleighEquation(matrix, approximation);
+			System.out.println(eigenvalue);
+			
+			iterations++;
+		//}
 	}
 	
 	public static void inversePowerMethod(Matrix matrix) {
 		//
+	}
+	
+	public static double rayleighEquation(Matrix matrix, Matrix eigenvector) {
+		Matrix rayleighNominator = (matrix.times(eigenvector)).transpose().times(eigenvector);
+		Matrix rayleighDenominator = eigenvector.transpose().times(eigenvector);
+		
+		return rayleighNominator.getArray()[0][0] / rayleighDenominator.getArray()[0][0];
 	}
 }
