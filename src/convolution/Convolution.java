bbc7043af35d63ab170c55d86f8d31f646636a8c
Ryan Ashcraft <ryanashcraft@gmail.com>
2011-04-10 15:35:01 -0400

    New convolution package by Ryan.

---
+++ src/convolution/Convolution.java
@@ -0,0 +1,76 @@
+package convolution;
+
+import jama.Matrix;
+
+public class Convolution {
+	public static void main(String[] args) {
+		convolude(new Matrix(new double[][]{{1.0, 0.0, 1.0, 1.0, 0.0}}).transpose());
+	}
+	
+	public static String convolude(Matrix x) {
+		for (int t = 0; t < x.getRowDimension()+3; t++) {
+			Matrix xPart = null;
+			
+			if (t >= x.getRowDimension()+1) {
+				xPart = new Matrix(new double[][] {
+						{x.get(t-3, 0),
+							0.0,
+							0.0,
+							0.0}}).transpose();
+			} else if (t >= x.getRowDimension()) {
+				xPart = new Matrix(new double[][] {
+						{x.get(t-3, 0),
+							x.get(t-2, 0),
+							0.0,
+							0.0}}).transpose();
+			} else if (t >= x.getRowDimension()-1) {
+				xPart = new Matrix(new double[][] {
+						{x.get(t-3, 0),
+							x.get(t-2, 0),
+							x.get(t-1, 0),
+							0.0}}).transpose();
+			} else if (t-3 >= 0) {
+				xPart = new Matrix(new double[][] {
+						{x.get(t-3, 0),
+							x.get(t-2, 0),
+							x.get(t-1, 0),
+							x.get(t, 0)}}).transpose();
+			} else if (t-2 >= 0) {
+				xPart = new Matrix(new double[][] {
+						{0.0,
+							x.get(t-2, 0),
+							x.get(t-1, 0),
+							x.get(t, 0)}}).transpose();
+			} else if (t-1 >= 0) {
+				xPart = new Matrix(new double[][] {
+						{0.0,
+							0.0,
+							x.get(t-1, 0),
+							x.get(t, 0)}}).transpose();
+			} else if (t >= 0){
+				xPart = new Matrix(new double[][] {
+						{0.0,
+							0.0,
+							0.0,
+							x.get(t, 0)}}).transpose();
+			}
+			
+			Matrix y0 = new Matrix(new double[][]{{1.0, 1.0, 0.0, 1.0}});
+			Matrix y1 = new Matrix(new double[][]{{1.0, 0.0, 1.0, 1.0}});
+			Matrix a = new Matrix(2, y0.getColumnDimension());
+			a.setMatrix(0, 0, 0, a.getColumnDimension()-1, y0);
+			a.setMatrix(1, 1, 0, a.getColumnDimension()-1, y1);
+			
+			Matrix result = a.times(xPart);
+			
+			for (int r = 0; r < result.getRowDimension(); r++) {
+				for (int c = 0; c < result.getColumnDimension(); c++) {
+					result.set(r, c, result.get(r, c)%2);
+				}
+			}
+			
+			result.print(10, 10);
+		}
+		return null;
+	}
+}
